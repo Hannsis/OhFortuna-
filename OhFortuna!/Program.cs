64c76f4cad15d2ec101f3b336f34e624d96da130
Hanna Madsen <hannamymadsen89@gmail.com>
2022-09-18 10:46:12 +0200

    metoder i match och dice

--- OhFortuna!/Program.cs
+++ OhFortuna!/Program.cs
@@ -10,6 +10,7 @@ först del a, sen implentera regler efter spelet. man skriver i den ordningen ma
 using System;
 using System.Collections.ObjectModel;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 
 namespace OhFortuna_
 {
@@ -17,36 +18,34 @@ namespace OhFortuna_
     {
         static void Main(string[] args)
         {
-           
-            bool keepPlaying = true;
-           
-           
-            //menyval - spela eller få instruktioner
-            //Eftersom loopen ska undersöka variabeln som innehåller användarensval så måste denna deklareras innan loopen.
-            string inputVal = "";
 
-            while (inputVal != "3") //all kod som man vill ska upprepas ska måste ligga i en loop.
+            bool keepPlaying = true;
+
+            string inputVal = "";
+            while (inputVal != "3")
             {
-                Console.WriteLine("Välj ett av följande val: ");
-                Console.WriteLine("1. Få spelets regler");
-                Console.WriteLine("2. Spela spelet");
-                Console.WriteLine("3. Avsluta spelet");
+
+                Menu();
+
 
                 inputVal = (Console.ReadLine());
 
                 switch (inputVal)
                 {
                     case "1":
+
                         SpelIntro();
                         break;
 
                     case "2":
+
                         int pix = 500;
                         Console.WriteLine($"You have {pix}");
+
                         keepPlaying = true;
+
                         while (keepPlaying)
                         {
-                           
                             int pixGambled = 0;
                             while (true)
                             {
@@ -62,17 +61,21 @@ namespace OhFortuna_
                                         break;
                                     }
                                     else
-                                        Console.WriteLine("Fel input");//Hur breakar jag ut loopen????
+                                        Console.WriteLine("Fel input");
                                 }
 
                             }
+                            /*
+                            Random random = new Random();
+                            
+                            List<int> Dice = new();     
+                            Dice.Add(random.Next(1, 7));
+                            Dice.Add(random.Next(1, 7));
+                            Dice.Add(random.Next(1, 7));
+                            */
 
-                            Random random = new Random();//generate random number
-                            List<int> Dice = new();
-                            Dice.Add(random.Next(1, 7));
-                            Dice.Add(random.Next(1, 7));
-                            Dice.Add(random.Next(1, 7));
 
+                            int match = 0;
                             int luckyNumber = 0;
                             while (true)
                             {
@@ -82,28 +85,21 @@ namespace OhFortuna_
                                 if (int.TryParse(input, out luckyNumber))
                                 {
                                     Console.WriteLine();
-                                    if (luckyNumber >= 1 && luckyNumber <= 7)
+                                    if (luckyNumber >= 1 && luckyNumber <= 6)
                                     {
                                         keepPlaying = true;
                                         break;
                                     }
                                     else
-                                        Console.WriteLine("Fel input");//Hur breakar jag ut loopen????
+                                        Console.WriteLine("This is a dice, it only rolls between 1 and 6.");
                                 }
 
                             }
-                         
-                            Console.WriteLine($"\r\nThe dice shows: {Dice[0]}, {Dice[1]} and {Dice[2]}");//vill ha nya randoms FIXAT
-
-                            int match = 0;
-                            for (int roll = 0; roll < Dice.Count; roll++)//deklarerar, kontrollerar, bearbetar (one time use).
-                            {
-                                if (Dice[roll] == luckyNumber)
-                                    match++;
-                            }
-
+                            match = RandomGen(luckyNumber, match);
+                            
                             pix = pix - pixGambled;
-                            if (match == 3)//4 ggr bet 
+
+                            if (match == 3)
                             {
 
                                 Console.WriteLine($"You got {match} of the dice right");
@@ -111,12 +107,12 @@ namespace OhFortuna_
                                 Console.WriteLine($"You won FOUR times the money, you now have: {pix}");
 
                             }
-                            else if (match == 2)//3 ggr bet
+                            else if (match == 2)
                             {
                                 Console.WriteLine($"You got {match} of the dice right");
                                 pix += (pixGambled * 3);
                             }
-                            else if (match == 1)//2 ggr bet
+                            else if (match == 1)
                             {
                                 Console.WriteLine($"You got {match} of the dice right");
                                 pix += (pixGambled * 2);
@@ -124,7 +120,6 @@ namespace OhFortuna_
                             else
                                 Console.WriteLine($"You got {match} of the dice right");
 
-                            Console.WriteLine();
 
 
                             Console.WriteLine($"You have {pix} left");
@@ -132,6 +127,7 @@ namespace OhFortuna_
                             if (pix < 50)
                             {
                                 Console.WriteLine("Sorry, you don't have enough to gamble with.");
+
                                 break;
                             }
 
@@ -143,11 +139,15 @@ namespace OhFortuna_
 
                             if (responses == ("Y"))
                             {
+                                Console.Clear();
+                                Console.WriteLine($"You have {pix} left");
                                 continue;
+
                             }
                             else
                             {
                                 keepPlaying = false;
+                                Console.Clear();
                                 break;
                             }
                         }
@@ -163,15 +163,41 @@ namespace OhFortuna_
                         break;
                 }
             }
-           
+
             static void SpelIntro()
             {
                 Console.WriteLine("Welcome to Pay'N'Play, the place where you get rich or die trying!");
-                Console.WriteLine("Your total Pixx is 500, and you must gamble at least 50 per turn");
+                Console.WriteLine("Your total Pix is 500, and you must gamble at least 50 per turn");
                 Console.WriteLine("            o If one of the dice shows your lucky number you get DOUBLE the pix\r\n            o At two dice TRIPPLE the pix\r\n            o If all three dice show your lucky number, FOUR times the pix");
                 Console.WriteLine("Good luck\n\n");
-            }        
-           
+
+            }
+            static void Menu()
+            {
+                Console.WriteLine("Välj ett av följande val: ");
+                Console.WriteLine("1. Få spelets regler");
+                Console.WriteLine("2. Spela spelet");
+                Console.WriteLine("3. Avsluta spelet");
+
+            }
+            static int RandomGen(int luckyNumber, int match)
+            {
+                Random random = new Random();
+
+                List<int> Dice = new();
+                Dice.Add(random.Next(1, 7));
+                Dice.Add(random.Next(1, 7));
+                Dice.Add(random.Next(1, 7));
+                Console.WriteLine($"\r\nThe dice shows: {Dice[0]}, {Dice[1]} and {Dice[2]}");
+
+                for (int i = 0; i < Dice.Count; i++)
+                {
+                    if (Dice[i] == luckyNumber)
+                        match++;
+                }
+                return match;
+
+            }
         }
     }
 }
